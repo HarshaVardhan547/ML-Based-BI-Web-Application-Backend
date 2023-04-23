26e08ad8979287839393b1e4a291cdd66a5b4099
harshavardhan <harshavardhan3e7@gmail.com>
2023-04-22 22:37:01 -0400

    new firebase code to make secure credentials file hiding

--- MyUtils/Firebase.py
+++ MyUtils/Firebase.py
@@ -8,10 +8,12 @@ from firebase_admin import credentials, firestore, storage
 
 
 def initialize_firestore():
-    cred = credentials.ApplicationDefault()
-    firebase_admin.initialize_app(cred, {
-        'projectId': os.environ['FIREBASE_PROJECT_ID'],
+    cred = credentials.Certificate({
+        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
     })
+    firebase_admin.initialize_app(cred)
 
 
 def get_firestore_files(search_file_name):
