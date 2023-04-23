a9f3cc86c083547aa8e54e2b0470f53605475b61
harshavardhan <harshavardhan3e7@gmail.com>
2023-04-22 23:07:10 -0400

    latest firebase

--- MyUtils/Firebase.py
+++ MyUtils/Firebase.py
@@ -6,7 +6,6 @@ import firebase_admin
 from dotenv import load_dotenv
 from firebase_admin import credentials, firestore, storage
 
-
 def initialize_firestore():
     cred = credentials.Certificate({
         "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
@@ -14,7 +13,6 @@ def initialize_firestore():
         "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
     })
     firebase_admin.initialize_app(cred)
-
 
 def get_firestore_files(search_file_name):
     try:
@@ -28,7 +26,6 @@ def get_firestore_files(search_file_name):
         limit(10). \
         stream()
     return docs
-
 
 def upload_file_to_firestore(file):
     try:
@@ -55,3 +52,12 @@ def upload_file_to_firestore(file):
         print(e)
         traceback.print_exc()
         return None
+
+if __name__ == '__main__':
+    load_dotenv() # load environment variables from .env file
+    cred = credentials.Certificate({
+        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    })
+    firebase_admin.initialize_app(cred)
